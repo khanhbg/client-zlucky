fd3fdb3fc4bf468025754b0cb68c0b8b6131574d
khanhbg <105843389+khanhbg@users.noreply.github.com>
2023-01-18 23:31:22 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,15 @@ import { LuckyWheelComponent } from '../app/lucky-wheel/lucky-wheel.component';
 import { HomeComponent } from '../app/home/home.component';
 import { RegisterComponent } from '../app/register/register.component';
 import {ProfileComponent} from '../app/profile/profile.component';
+import {TimelineComponent} from '../app/timeline/timeline.component';
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   { path: 'luckyWheel', component: LuckyWheelComponent},
   { path: 'profile', component: ProfileComponent},
+  { path: 'profile', component: ProfileComponent},
+  { path: 'timeline', component: ProfileComponent},
   { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
 
